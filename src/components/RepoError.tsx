5e83f417e70252afce4f4bbf7e2176142af3f58d
Manish Kumar Das <ammanishkumardas@gmail.com>
2023-06-01 15:25:51 +0530

    repo error handling

--- src/components/RepoError.tsx
+++ src/components/RepoError.tsx
@@ -1,25 +1,12 @@
 import React, { FC } from 'react'
+import styles from './UserDataError.module.css'
 
 type Props = {}
 
 const RepoError: FC<Props> = () => {
   return (
-    <div>RepoError</div>
-  )
-}
-
-const NotFound = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
-
-const Error = () => {
-  return (
-    <div>
-
+    <div className={styles.not_found__main}>
+      <h1>Couldn't fetch User Details</h1>
     </div>
   )
 }
